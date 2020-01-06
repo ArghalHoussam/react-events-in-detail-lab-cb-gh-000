06316092ee812fb3f114e69041ce448216fbab28
ossama abd rahman <houssam.arghal1997@gmail.com>
2020-01-06 12:00:57 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,12 @@
+// Code CoordinatesButton Component Here
+import React from 'react';
+class DelayedButton extends React.Component {
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+export default DelayedButton;
 // Code DelayedButton Component Here
